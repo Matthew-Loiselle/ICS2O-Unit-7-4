9dde12ab5f417ba5f687f5990737da45a5b498ca
MatthewLoiselle <>
2021-05-08 04:23:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let userAge = 0
+let userDay = 0
+document.getElementById('start').addEventListener('click', goodMorning)
+
+function goodMorning () {
+  userAge = document.getElementById('age').value
+  userDay = document.getElementById('day').value
+  if ((userDay == 'saturday' || userDay == 'sunday') && (userAge < 18)) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend!'
+  } else if ((userDay == 'saturday' || userDay == 'sunday') && (userAge > 17)) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend!'
+  } else if ((userDay != 'saturday' || userDay != 'sunday') && (userAge < 18)) {
+    document.getElementById('answer').innerHTML = 'Time to get up for school'
+  } else if ((userDay != 'saturday' || userDay != 'sunday') && (userAge > 17)) {
+    document.getElementById('answer').innerHTML = 'Time to get up for work'
+  }
+}
